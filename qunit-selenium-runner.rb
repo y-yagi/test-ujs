8f19225828cb6ef547edf7e17ad175baf93bbd8c
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-06-09 11:40:09 +0900

    no need?

--- qunit-selenium-runner.rb
+++ qunit-selenium-runner.rb
@@ -3,7 +3,7 @@
 require "qunit/selenium/test_runner"
 require "chromedriver/helper"
 
-spawn("chromedriver")
+# spawn("chromedriver")
 
 driver_options = Selenium::WebDriver::Chrome::Options.new
 driver_options.add_argument("--headless")
